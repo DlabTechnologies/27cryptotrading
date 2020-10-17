5edb9b80f2b90574c6112b06ec5a10ba1d31008c
Universe Explorerblog <universeexplorerblog24@gmail.com>
2020-10-17 22:31:06 +0100

    commit message

--- cryptotrading/settings.py
+++ cryptotrading/settings.py
@@ -161,7 +161,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-
 #ssl redirect
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -175,7 +174,7 @@ SECURE_SSL_REDIRECT = True
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static'),
+#   os.path.join(BASE_DIR, 'static'),
 #]
 
 #MEDIA_URL = '/media/'
